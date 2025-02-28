2d03347acbb8449c29480c70a8755900ddce6daa
Chen Jiaxin <chenjiaxin@shopee.com>
2025-02-28 21:52:53 +0800

    termuxdc

--- backend/termuxdc/allocator.c
+++ backend/termuxdc/allocator.c
@@ -65,7 +65,7 @@ static bool begin_data_ptr_access(struct wlr_buffer *wlr_buffer,
     }
 
     *data = buffer->data;
-    *format = buffer->termuxdc_buffer_ptr->format;∏
+    *format = buffer->termuxdc_buffer_ptr->format;
     *stride = buffer->termuxdc_buffer_ptr->desc.stride;
     return true;
 }
