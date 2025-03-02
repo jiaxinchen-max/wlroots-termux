704251f0b8f30ecb0b343ec9d4d4e557c2317e81
Chen Jiaxin <chenjiaxin@shopee.com>
2025-03-02 23:12:17 +0800

    termuxdc

--- backend/termuxdc/allocator.c
+++ backend/termuxdc/allocator.c
@@ -73,6 +73,7 @@ static bool begin_data_ptr_access(struct wlr_buffer *wlr_buffer,
 static void end_data_ptr_access(struct wlr_buffer *wlr_buffer) {
     struct wlr_termuxdc_buffer *buffer = termuxdc_buffer_from_buffer(wlr_buffer);
     if (buffer->data) {
+        buffer->termuxdc_buffer_ptr->begin_draw(&buffer->data);
         buffer->termuxdc_buffer_ptr->end_draw();
         buffer->data = NULL;
     }
