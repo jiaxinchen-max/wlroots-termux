578c8a7f0cb3faecb76b01611d4ab5eb1726c6bd
Chen Jiaxin <chenjiaxin@shopee.com>
2025-03-02 17:35:56 +0800

    termuxdc

--- backend/termuxdc/output.c
+++ backend/termuxdc/output.c
@@ -264,7 +264,7 @@ struct wlr_output *wlr_termuxdc_output_create(struct wlr_backend *wlr_backend) {
     termuxdc_wlr_queue_init(&output->present_queue);
     termuxdc_wlr_queue_init(&output->idle_queue);
 
-    pthread_create(&output->present_thread, NULL, present_queue_thread, output);
+    // pthread_create(&output->present_thread, NULL, present_queue_thread, output);
 
     wl_signal_emit_mutable(&backend->backend.events.new_output, wlr_output);
 
