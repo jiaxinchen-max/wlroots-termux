7c215652bbac5f8c1f7873c33e062f98a9bd1045
Chen Jiaxin <chenjiaxin@shopee.com>
2025-03-12 20:36:31 +0800

    termuxdc

--- backend/termuxdc/output.c
+++ backend/termuxdc/output.c
@@ -170,7 +170,7 @@ int handle_termuxdc_server_timer_event(struct wlr_termuxdc_output *output) {
         wlr_output_send_frame(&output->wlr_output);
     }
 
-    return 0;
+    return 1;
 }
 
 static void *present_queue_thread(void *data) {
@@ -247,7 +247,7 @@ struct wlr_output *wlr_termuxdc_output_create(struct wlr_backend *wlr_backend) {
     //     return NULL;
     // }
 
-    int ret = display_client_init(1920,1080,4);
+    int ret = display_client_init(1080,1920,4);
     if (ret!=TERMUX_DC_OK){
         wlr_log(WLR_ERROR, "Failed to init display client");
     }
@@ -256,7 +256,7 @@ struct wlr_output *wlr_termuxdc_output_create(struct wlr_backend *wlr_backend) {
     wlr_output_state_init(&state);
     wlr_output_state_set_render_format(&state, DRM_FORMAT_ABGR8888);
     wlr_output_state_set_transform(&state, WL_OUTPUT_TRANSFORM_FLIPPED_180);
-    wlr_output_state_set_custom_mode(&state, 1920, 1080, DEFAULT_REFRESH);
+    wlr_output_state_set_custom_mode(&state, 1080, 1920, DEFAULT_REFRESH);
     wlr_output_init(&output->wlr_output, &backend->backend, &output_impl, backend->loop,
                     &state);
     wlr_output_state_finish(&state);
