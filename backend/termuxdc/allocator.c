7ca2ef978d4d576ebfe636ea093d99587de9fa0d
Chen Jiaxin <chenjiaxin@shopee.com>
2025-03-19 20:40:31 +0800

    termuxdc

--- backend/termuxdc/allocator.c
+++ backend/termuxdc/allocator.c
@@ -166,13 +166,13 @@ static struct wlr_buffer *allocator_create_buffer(struct wlr_allocator *wlr_allo
     }
     wlr_log(WLR_DEBUG, "success to get dmabuf");
     buffer->dmabuf = (struct wlr_dmabuf_attributes) {
-        .width = buffer->termuxdc_buffer_ptr->desc.stride,
+        .width = buffer->termuxdc_buffer_ptr->desc.stride*4,
         .height = buffer->termuxdc_buffer_ptr->desc.height,
         .n_planes = 1,
         .format = format->format,
         .modifier = DRM_FORMAT_MOD_LINEAR,
         .offset[0] = 0,
-        .stride[0] = buffer->termuxdc_buffer_ptr->desc.stride,
+        .stride[0] = buffer->termuxdc_buffer_ptr->desc.stride * 4,
         .fd[0] = fd,
 
     };
