34f4c3580113343451dc983df519d4a2913e2f40
Chen Jiaxin <chenjiaxin@shopee.com>
2025-03-30 22:34:37 +0800

    termuxdc

--- backend/termuxdc/output.c
+++ backend/termuxdc/output.c
@@ -233,7 +233,7 @@ struct wlr_output *wlr_termuxdc_output_create(struct wlr_backend *wlr_backend) {
     // }
 
     // int ret = display_client_init(1024,768,4);
-    int ret = display_client_init(768,1024,4);
+    int ret = display_client_init(1080,2274,4);
     if (ret!=TERMUX_DC_OK){
         return NULL;
     }
