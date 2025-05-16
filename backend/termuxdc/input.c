ab0451a1e8f5bdf06fd3dd0108ad006d63d25c57
jiaxinchen-max <jiaxinchen5712@gmail.com>
2025-05-16 22:10:52 +0800

    termuxdc

--- backend/termuxdc/input.c
+++ backend/termuxdc/input.c
@@ -150,13 +150,12 @@ void handle_termuxdc_mouse_event(termuxdc_event *e, struct wlr_termuxdc_output *
             double y = (double) e->mouse.y / output->wlr_output.height;
             double px = (double) 1 / output->wlr_output.width;
             double py = (double) 1 / output->wlr_output.height;
-            double dx = output->touch_pointer.x - x;
-            double dy = output->touch_pointer.y - y;
-            if (dx >= px || dx <= -px || dy >= py || dy <= -py) {
-                output->touch_pointer.x -= dx;
-                output->touch_pointer.y -= dy;
+        
+            if (x >= px || x <= -px || y >= py || y <= -py) {
+                output->touch_pointer.x -= x;
+                output->touch_pointer.y -= y;
             }
-            move_cursor(output, dx, dy, time_ms);
+            move_cursor(output, x, y, time_ms);
         break;
     } 
     case TDC_MOUSE_LEFT_BUTTON:{
